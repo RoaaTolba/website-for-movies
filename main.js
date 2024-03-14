063064e16baefb424b2139a257a3325f4a9d70c3
Roaa Tolba <roaatolba908@gmail.com>
2024-03-14 10:55:49 +0200

    Final result with full code integration"

--- main.js
+++ main.js
@@ -1,15 +1,15 @@
-var swiper = new Swiper(".swiper-container", {
-  spaceBetween: 30,
-  centeredSlides: true,
-  autoplay: {
-    delay: 2500,
-    disableOnInteraction: false,
-  },
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-  }
-});
+// var swiper = new Swiper(".home", {
+//   spaceBetween: 30,
+//   centeredSlides: true,
+//   autoplay: {
+//     delay: 2500,
+//     disableOnInteraction: false,
+//   },
+//   pagination: {
+//     el: ".swiper-pagination",
+//     clickable: true,
+//   }
+// });
 
 var swiper = new Swiper(".coming-container", {
   spaceBetween: 20,
